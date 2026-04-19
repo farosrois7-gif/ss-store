0921b27759f4ee7ee2a6fecdb192467bf12135fb
Rizki Ahmad Fadhilah <rizkiahmadfadhilah2@gmail.com>
2026-04-19 17:56:43 +0700

    fix: dynamic port for production

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ dotenv.config();
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
